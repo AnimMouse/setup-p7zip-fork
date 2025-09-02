d7164bcf2c260ca16814bc2d03b30dbe021b7fd1
Anim Mouse <git@animmouse.com>
2025-09-03 05:17:00 +0800

    feat: Only install 7za standalone

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,5 +3,5 @@ set -eu
 echo ::group::Downloading p7zip fork $version for $RUNNER_OS
 mkdir p7zip-fork
 wget -q $GITHUB_SERVER_URL/p7zip-project/p7zip/releases/download/$version/linux-p7zip.zip 
-unzip -q linux-p7zip.zip -d p7zip-fork
+unzip -q linux-p7zip.zip 7za -d p7zip-fork
 echo ::endgroup::
