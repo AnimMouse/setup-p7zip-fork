b53357e5cf274e9903e10d5b9a2b63a879b18504
Anim Mouse <git@animmouse.com>
2025-09-03 04:56:32 +0800

    refactor: Use AnimMouse/tool-cache, add automatic version, and fix URL

---
+++ scripts/download.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -eu
+echo ::group::Downloading p7zip fork $version for $RUNNER_OS
+mkdir p7zip-fork
+wget -q $GITHUB_SERVER_URL/p7zip-project/p7zip/releases/download/$version/linux-p7zip.zip 
+unzip -q linux-p7zip.zip -d p7zip-fork
+echo ::endgroup::
